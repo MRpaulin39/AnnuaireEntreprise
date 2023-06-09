e9710ba6b9fd4a83d7f9c2aba49bb2320125c6c4
Paulin Buland <buland2001@gmail.com>
2023-06-09 13:03:02 +0200

    Ajout injection de dépendance

--- AnnuaireEntreprise/App.xaml.cs
+++ AnnuaireEntreprise/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using AnnuaireEntreprise.Core.Application.Repositories;
+using AnnuaireEntreprise.Core.Infrastructure.DataLayers;
+using AnnuaireEntreprise.Core.Interfaces.Infrastructure;
+using AnnuaireEntreprise.Core.Interfaces.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Windows;
 
 namespace AnnuaireEntreprise
 {
@@ -10,6 +16,36 @@ namespace AnnuaireEntreprise
         public App()
         {
 
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(typeof(MainWindow));
+
+            #region Résolution des interfaces
+            #region DataLayers
+            services.AddScoped<IEmployeeDataLayer, EmployeeDataLayer>();
+            services.AddScoped<IServiceDataLayer, ServiceDataLayer>();
+            services.AddScoped<ISiteDataLayer, SiteDataLayer>();
+            #endregion
+
+            #region Repositories
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IServiceRepository, ServiceRepository>();
+            services.AddScoped<ISiteRepository, SiteRepository>();
+            #endregion
+            #endregion
         }
 
     }
